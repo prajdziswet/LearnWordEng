47220697813b9c8fcd86c11c0721ef387c7c2fd4
Уладзімір Косаў <prajdziswet@mail.ru>
2022-08-05 10:33:56 +0300

    changeDB

--- EnglishWord7000/Controllers/HomeController.cs
+++ EnglishWord7000/Controllers/HomeController.cs
@@ -19,25 +19,48 @@ namespace EnglishWord7000.Controllers
 
         public IActionResult Index()
         {
-
+            //Console.WriteLine(dbContext.Words.Count());
             //if (dbContext.Words.Count() == 0)
             //{
-            //    CreateList cr = new CreateList();
-            //    //dbContext.Words.RemoveRange(dbContext.Words);
-            //    dbContext.Words.AddRange(cr.Words);
-            //    dbContext.SaveChanges();
+            //    List<Word> allWords = new List<Word>();
+            //    var group=dbContext.Words.Where(x=>x.WordEng.Level!=null).Include(x=>x.WordRu).Include(x=>x.WordEng).ToList().GroupBy(x=>x.WordEng.Level).OrderBy(x=>x.Key).ToDictionary(x=>x.Key,x=>x.ToList());
+
+            //    List<string> consolewrite = new List<string>();
+            //   foreach (var levelgroup in group)
+            //   {
+            //       if (levelgroup.Key != null)
+            //       {
+            //           var wordanother= dbContext.Words.Include(x => x.WordRu).Include(x => x.WordEng).ToList().Where(x => levelgroup.Value.Any(y => x.WordEng.Word == y.WordEng.Word && x.WordEng.Level != y.WordEng.Level)).ToList();
+            //           levelgroup.Value.ShuffleInPlace();
+            //           AddnewWords(levelgroup.Value,allWords);
+            //           AddnewWords(wordanother, allWords);
+            //       }
+
+            //   }
+            //   dbContext.Words.RemoveRange(dbContext.Words);
+            //   dbContext.SaveChanges();
+            //   Console.WriteLine("all:  " + allWords.Count);
+            //   dbContext.Words.AddRange(allWords);
+            //   dbContext.SaveChanges();
             //}
+            //Console.WriteLine(dbContext.Words.Count());
 
             return View();
         }
 
-        [Authorize]
-        public IActionResult Learn()
-        {
-            return View();
-        }
+        //private void AddnewWords(IList<Word> addList,List<Word> allword)
+        //{
+        //    foreach (var element in addList)
+        //    {
+        //        Word newWord=new Word();
+        //        newWord.WordEng = element.WordEng;
+        //        newWord.WordRu = element.WordRu;
+        //        newWord.CheckWords =element.CheckWords;
+        //        allword.Add(newWord);
+        //    }
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
